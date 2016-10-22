c01e4b0bbdfe4f0f1aed1512e8478740c6306f8b
Jay Seo <jonghwa.seo@gmail.com>
2016-10-22 15:25:36 +0900

    Remove unused files

--- bsp/uart.c
+++ bsp/uart.c
@@ -1,6 +1,3 @@
-#include "reg_macro.h"
-#include "artik_board_config.h"
-
 #define UART_OFFSET	0x10000
 
 #define	DEBUG_UART_CH	1
@@ -19,39 +16,9 @@
 #define UARTFR 			0x06
 #define UARTFR_TXFF 	0x20
 
-
-void uart_init(void)
-{
-	writel(0x11220000, GPP0_CON);
-
-	// baud rate setting 115200
-	writel(0x0d, UBRDIVn(DEBUG_UART_CH));
-	writel(0x0b, UFRACVALn(DEBUG_UART_CH));
-}
-
-char uart_getc_poll(void)
-{
-	unsigned long ch; 
-	while(!(readl(UFSTATn(DEBUG_UART_CH)) & 0xff));
-	ch = URXHn(DEBUG_UART_CH);
-
-	return (char)ch; 
-}
-
 int uart_putc_poll(char c)
 {
-#if 0
-	if(c == '\n') {
-		while (readl(UFSTATn(DEBUG_UART_CH)) & TX_FIFO_FULL);
-		writel('\r', UTXHn(DEBUG_UART_CH));
-	}
-
-	while (readl(UFSTATn(DEBUG_UART_CH)) & TX_FIFO_FULL);
-	writel(c, UTXHn(DEBUG_UART_CH));
-	return c;
-#else
 	while(*(UART0 + UARTFR) & UARTFR_TXFF);
 	*UART0 = c;
-#endif		
 }
 
--- os/fos_timer.h
+++ os/fos_timer.h
@@ -2,43 +2,6 @@
 #define OS_TIMER_H
 
 #include "bsp.h"
-#include "artik_board_config.h"
-#include "irqs.h"
-#include "gic.h"
-
-#if 0
-#define MCT_CH_OFFSET		0x100
-
-#define MCT_L_TCNTCB(ch)	(MCT_L_BASE + (MCT_CH_OFFSET * ch) + 0x00)
-#define MCT_L_TCNTCO(ch)	(MCT_L_BASE + (MCT_CH_OFFSET * ch) + 0x04)
-#define MCT_L_TCON(ch)		(MCT_L_BASE + (MCT_CH_OFFSET * ch) + 0x20)
-#define		L_TCON_FRC_TIMER	 											0x08
-#define		L_TCON_INT_TYPE_ONESHOT											0x04
-#define		L_TCON_INT_START												0x02
-#define		L_TCON_TIMER_													0x01
-#define MCT_L_INT_CSTAT(ch)	(MCT_L_BASE + (MCT_CH_OFFSET * ch) + 0x30)
-#define MCT_L_INT_ENB(ch)	(MCT_L_BASE + (MCT_CH_OFFSET * ch) + 0x34)
-#define MCT_L_WSTAT(ch)		(MCT_L_BASE + (MCT_CH_OFFSET * ch) + 0x40)
-
-#define MCT_L_FRCNTB(ch)	(MCT_L_BASE + (MCT_CH_OFFSET * ch) + 0x10)
-#define MCT_L_FRCNTO(ch)	(MCT_L_BASE + (MCT_CH_OFFSET * ch) + 0x14)
-
-#define MCT_COMP_OFFSET		0x10
-#define MCT_G_CNTL			(MCT_G_BASE)
-#define MCT_G_CNTU			(MCT_G_BASE + 0x004)
-#define MCT_G_CNT_WSTAT		(MCT_G_BASE + 0x010)
-#define MCT_G_COMP_L(ch)	(MCT_G_BASE + 0x100 + MCT_COMP_OFFSET*ch + 0x00)
-#define MCT_G_COMP_U(ch)	(MCT_G_BASE + 0x100 + MCT_COMP_OFFSET*ch + 0x04)
-#define MCT_G_AUTO_INC(ch)	(MCT_G_BASE + 0x100 + MCT_COMP_OFFSET*ch + 0x08)
-#define MCT_G_TCON			(MCT_G_BASE + 0x140)
-#define 	MCT_G_TCON_ENABLE			0x100
-#define		MCT_G_TCON_AUTO_INC(ch)		(0x2 << (ch*2))
-#define		MCT_G_TCON_COMP_EN(ch)		(0x1 << (ch*2))
-#define	MCT_G_INT_CSTAT		(MCT_G_BASE + 0x144)
-#define	MCT_G_INT_ENB		(MCT_G_BASE + 0x148)
-#define	MCT_G_WSTAT			(MCT_G_BASE + 0x14C)
-
-#else
 
 /* http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ddi0271d/index.html */
 //#define TIMER_OFFSET 			((volatile unsigned int*)0x101E2000
@@ -64,8 +27,6 @@
 #define PIC_TIMER01				0x10					/* RTC */
 /* http://infocenter.arm.com/help/topic/com.arm.doc.ddi0181e/I1006461.html */
 #define VIC_INTENABLE 			0x4 /* 0x10 bytes */
-#endif
-
 
 #define TIMER_1MS				1000
 #define TIMER_10MS				10000
